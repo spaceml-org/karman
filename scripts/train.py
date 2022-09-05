99b558a63f3c60f73b9c252d24372b82a532b863
Sceki <giacomo.acciarini@gmail.com>
2022-09-05 12:35:12 +0200

    add more options for the network

--- scripts/train.py
+++ scripts/train.py
@@ -164,9 +164,9 @@ def run():
             model = Fism2FlareDensityPredictor(
                 input_size_thermo=dataset.data_thermo['data_matrix'].shape[1],
                 input_size_fism2_flare=dataset.time_series_data['fism2_flare_stan_bands']['data_matrix'].shape[1],
-                output_size_fism2_flare=175,
-                dropout_lstm=0.,
-                dropout_ffnn=0.
+                output_size_fism2_flare=opt.out_features,
+                dropout_lstm=opt.dropout,
+                dropout_ffnn=opt.dropout
             )
 
 

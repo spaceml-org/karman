67a3f30cf05923276296b393d999502c4cd3dda2
Giacomo Acciarini <giacomo.acciarini@gmail.com>
2023-09-05 10:55:07 +0200

    Delete tests/test_benchmark.py

--- tests/test_benchmark.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from io import StringIO
-
-class BenchmarkTestCases(unittest.TestCase):
-    def test_benchmark(self):    
-        #make sure that the benchmark is indeed
-        #capturin the correct altitude, geomagnetic storm conditions
-        #and it's computing the metrics properly
-        self.assertTrue(1==1)

8785ebe6cd188736e21603e31c5e807da496e9ec
Giacomo Acciarini <giacomo.acciarini@gmail.com>
2023-09-05 11:02:29 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ sys.path.insert(0, os.path.abspath('../../karman'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'Kessler'
+project = 'Karman'
 copyright = '2023, Karman contributors'
 author = 'Karman contributors'
 
